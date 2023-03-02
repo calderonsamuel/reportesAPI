ebd270a6e2163af6b3cc4e28a85482dababbd0a8
calderonsamuel <samuel.calderon@uarm.pe>
2023-03-02 16:12:17 -0500

    ensure class User works as expected

--- R/class-User.R
+++ R/class-User.R
@@ -2,6 +2,7 @@
 #'
 #' R6 class that allows to get the user information.
 #'
+#' @param user_id ID of the user to edit or delete.
 #' @param email The email the user started the session with.
 #' @param name Name to be inserted as user metadata.
 #' @param last_name Last name to be inserted as user metadata.
@@ -18,15 +19,38 @@ User <- R6::R6Class(
             super$initialize()
             private$sync_user_data(email)
         },
+        #' @description Add a new user to the database
+        user_add = function(name, last_name, email) {
+            user_id <- ids::random_id()
+            statement <- "
+                INSERT INTO users
+                SET
+                    user_id = {user_id},
+                    name = {name},
+                    last_name = {last_name},
+                    email = {email}
+            "
+            super$db_execute_statement(statement, .envir = rlang::current_env())
+            return(user_id)
+        },
+        #' @description Delete a user from the database
+        user_delete = function(user_id) {
+            statement <- "
+                DELETE FROM users
+                WHERE user_id = {user_id}
+            "
+            super$db_execute_statement(statement, .envir = rlang::current_env())
+        },
         #' @description Change the name and last name of the User
-        user_edit_names = function(name, last_name) {
-            statement <-
-                "UPDATE users
+        user_edit_names = function(user_id, name, last_name) {
+            statement <-"
+                UPDATE users
                 SET
                     name = {name},
                     last_name = {last_name}
                 WHERE
-                    user_id = {self$user$user_id}"
+                    user_id = {user_id}
+            "
             super$db_execute_statement(statement, .envir = rlang::current_env())
             private$sync_user_data(self$user$email)
         }
@@ -54,25 +78,7 @@ User <- R6::R6Class(
             super$db_get_query(query, email = email) # should be 1 row
         },
         user_create = function(email) {
-            fields <- list(
-                id = ids::random_id(),
-                email = email
-            )
-            statement <- "
-                INSERT INTO users
-                SET
-                    user_id = {id},
-                    name = '',
-                    last_name = '',
-                    email = {email}"
-            super$db_execute_statement(statement, .envir = fields)
-        },
-        user_delete = function(user_id) {
-            statement <-
-                "DELETE FROM users
-                WHERE user_id = {id}"
-
-            super$db_execute_statement(statement, id = user_id)
+            self$user_add(name = '', last_name = '', email = email)
         }
     )
 )
