1bd98b3e272045d420a8f87d4f4a67d2b975c03a
calderonsamuel <samuel.calderon@uarm.pe>
2023-03-03 15:43:57 -0500

    add tests for Task

--- R/class-Task.R
+++ R/class-Task.R
@@ -3,9 +3,6 @@
 #' R6 class that allows to get the Group information.
 #'
 #' @param email The email the user started the session with.
-#' @param process_id The id of the process the task belongs to. When used must be paired with activity_id
-#' @param activity_id The id of the activity the task belongs to. When used must be paired with process_id
-#' @param org_id The id of the organisation on which the statement will be executed
 #' @param group_id The id of the group on which the statement will be executed
 #' @param user_id The id of the user on which the statement will be executed
 #' @param task_id The id of the task on which the statement will be executed
@@ -30,22 +27,16 @@ Task <- R6::R6Class(
         initialize = function(email) {
             super$initialize(email)
         },
-        #' @description Add a new task for an User
-        task_add = function(process_id = NA_character_, activity_id = NA_character_,
-                            org_id, group_id,
+        #' @description Add a new task for an User and report it as initial progress.
+        task_add = function(group_id,
                             task_title, task_description, assignee, time_due,
                             output_unit, output_goal) {
-
-            private$check_process(process_id, activity_id)
 
             task_id <- ids::random_id()
 
             statement <-
                 "INSERT INTO tasks
                 SET
-                    process_id = {process_id},
-                    activity_id = {activity_id},
-                    org_id = {org_id},
                     group_id = {group_id},
                     task_id = {task_id},
                     task_title = {task_title},
@@ -59,65 +50,37 @@ Task <- R6::R6Class(
                     status_current = 'Pendiente'
                 "
             super$db_execute_statement(statement, .envir = rlang::current_env())
-
-            cli::cli_h2("Task added")
-            cli::cli_alert_info("process_id: {process_id}")
-            cli::cli_alert_info("activity_id: {activity_id}")
-            cli::cli_alert_info("org_id: {org_id}")
-            cli::cli_alert_info("group_id: {group_id}")
-            cli::cli_alert_success("task_id: {task_id}")
-
-            self$progress_add(process_id, activity_id,
-                              org_id, group_id, task_id,
-                              output_progress = 0,
-                              status = "Pendiente",
-                              details = "Tarea iniciada") |>
-                suppressMessages()
-
+            
+            if (interactive()) {
+                cli::cli_h2("Task added")
+                cli::cli_alert_info("group_id: {group_id}")
+                cli::cli_alert_success("task_id: {task_id}")
+            }
+            
+            return(task_id)
         },
+        
         #' @description Delete an user task
-        task_delete = function(process_id = NA_character_, activity_id = NA_character_,
-                               org_id, group_id, task_id) {
-
-            private$check_process(process_id, activity_id)
+        task_delete = function(task_id) {
 
             statement <-
                 "DELETE FROM tasks
                 WHERE
-                    org_id = {org_id} AND
-                    group_id = {group_id} AND
                     task_id = {task_id}"
 
-            if (!is.na(process_id) && !is.na(activity_id)) {
-                statement <-
-                    paste0(statement,
-                        " AND process_id = {process_id} AND activity_id = {activity_id}")
-            }
-
             super$db_execute_statement(statement, .envir = rlang::current_env())
-
-            cli::cli_h2("Task deleted")
-            cli::cli_alert_info("process_id: {process_id}")
-            cli::cli_alert_info("activity_id: {activity_id}")
-            cli::cli_alert_info("org_id: {org_id}")
-            cli::cli_alert_info("group_id: {group_id}")
-            cli::cli_alert_danger("task_id: {task_id}")
-
-            self$progress_add(process_id, activity_id,
-                              org_id, group_id, task_id,
-                              output_progress = 0,
-                              status = "Eliminada",
-                              details = "Tarea eliminada") |>
-                suppressMessages()
+            
+            if (interactive()) {
+                cli::cli_h2("Task deleted")
+                cli::cli_alert_danger("task_id: {task_id}")
+            }
         },
 
         #' @description Report progress on an assigned task
-        task_report_progress = function(process_id = NA_character_,
-                                        activity_id = NA_character_,
-                                        org_id, group_id, task_id,
-                                        status_current, output_current, details) {
-
-            private$check_process(process_id, activity_id)
+        task_report_progress = function(task_id,
+                                        status_current, 
+                                        output_current, 
+                                        details) {
 
             statement <-
                 "UPDATE tasks
@@ -125,39 +88,25 @@ Task <- R6::R6Class(
                     status_current = {status_current},
                     output_current = {output_current}
                 WHERE
-                    org_id = {org_id} AND
-                    group_id = {group_id} AND
                     task_id = {task_id}"
 
-            if (!is.na(process_id) && !is.na(activity_id)) {
-                statement <-
-                    paste0(statement,
-                           " AND process_id = {process_id} AND activity_id = {activity_id}")
+            super$db_execute_statement(statement, .envir = rlang::current_env())
+            
+            if (interactive()) {
+                cli::cli_h2("Task edited")
+                cli::cli_alert_warning("task_id: {task_id}")
             }
 
-            super$db_execute_statement(statement, .envir = rlang::current_env())
-
-            cli::cli_h2("Task edited")
-            cli::cli_alert_info("process_id: {process_id}")
-            cli::cli_alert_info("activity_id: {activity_id}")
-            cli::cli_alert_info("org_id: {org_id}")
-            cli::cli_alert_info("group_id: {group_id}")
-            cli::cli_alert_warning("task_id: {task_id}")
-
-            self$progress_add(process_id, activity_id,
-                              org_id, group_id, task_id,
+            self$progress_add(task_id,
                               output_progress = output_current,
                               status = status_current,
-                              details = details) |>
-                suppressMessages()
+                              details = details)
         },
 
         #' @description Edit a task metadata
-        task_edit_metadata = function(process_id = NA_character_,
-                                      activity_id = NA_character_,
-                                      org_id, group_id, task_id,
-                                      task_title, task_description) {
-            private$check_process(process_id, activity_id)
+        task_edit_metadata = function(task_id,
+                                      task_title, 
+                                      task_description) {
 
             statement <-
                 "UPDATE tasks
@@ -165,46 +114,25 @@ Task <- R6::R6Class(
                     task_title = {task_title},
                     task_description = {task_description}
                 WHERE
-                    org_id = {org_id} AND
-                    group_id = {group_id} AND
                     task_id = {task_id}"
-
-            if (!is.na(process_id) && !is.na(activity_id)) {
-                statement <-
-                    paste0(statement,
-                           " AND process_id = {process_id} AND activity_id = {activity_id}")
-            }
 
             super$db_execute_statement(statement, .envir = rlang::current_env())
 
-            cli::cli_h2("Task edited")
-            cli::cli_alert_info("process_id: {process_id}")
-            cli::cli_alert_info("activity_id: {activity_id}")
-            cli::cli_alert_info("org_id: {org_id}")
-            cli::cli_alert_info("group_id: {group_id}")
-            cli::cli_alert_warning("task_id: {task_id}")
-
-            self$progress_add(process_id, activity_id,
-                              org_id, group_id, task_id,
-                              output_progress = NA,
-                              status = "Sin cambio",
-                              details = "Tarea con medatadata modificada") |>
-                suppressMessages()
+            if (interactive()) {
+                cli::cli_h2("Task edited")
+                cli::cli_alert_warning("task_id: {task_id}")
+            }
         },
+        
         #' @description Insert progress info on some task
-        progress_add = function(process_id = NA_character_,
-                                activity_id = NA_character_,
-                                org_id, group_id, task_id,
-                                output_progress, status, details) {
-            private$check_process(process_id, activity_id)
+        progress_add = function(task_id,
+                                output_progress, 
+                                status, 
+                                details) {
 
             statement <-
                 "INSERT INTO progress
                 SET
-                    process_id = {process_id},
-                    activity_id = {activity_id},
-                    org_id = {org_id},
-                    group_id = {group_id},
                     task_id = {task_id},
                     reported_by = {self$user$user_id},
                     output_progress = {output_progress},
@@ -212,14 +140,12 @@ Task <- R6::R6Class(
                     details = {details}"
 
             super$db_execute_statement(statement, .envir = rlang::current_env())
-
-            cli::cli_h2("Progress reported")
-            cli::cli_alert_info("process_id: {process_id}")
-            cli::cli_alert_info("activity_id: {activity_id}")
-            cli::cli_alert_info("org_id: {org_id}")
-            cli::cli_alert_info("group_id: {group_id}")
-            cli::cli_alert_warning("task_id: {task_id}")
-            cli::cli_alert_warning("status: {status}")
+            
+            if (interactive()) {
+                cli::cli_h2("Progress reported")
+                cli::cli_alert_warning("task_id: {task_id}")
+                cli::cli_alert_warning("status: {status}")
+            }
         },
         #' @description Get a task's progression history
         task_get_history = function(task_id) {
@@ -281,11 +207,6 @@ Task <- R6::R6Class(
             db_data |>
                 purrr::pmap(list) |>
                 setNames(nm = db_data$task_id)
-        },
-        check_process = function(process_id, activity_id) {
-            if (xor(is.na(process_id), is.na(activity_id))) {
-                rlang::abort("`process_id` y `activity_id` deben ser o NA o chr en simultaneo")
-            }
         },
         get_assignees = function() {
             current_group <- self$group_users[[self$group_selected]]
